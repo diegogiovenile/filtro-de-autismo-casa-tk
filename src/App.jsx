dac1495618123f794fb0f63da39272b35bb1aa19
Diego Sanchez <diegogiovenile@gmail.com>
2026-03-21 08:43:27 -0600

    fix eliminar historial

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,5 @@
-import { useState } from "react";
 import Evaluacion from "./Evaluacion";
-import Historial from "./Historial";
 
 export default function App() {
-
-const [pantalla, setPantalla] = useState("evaluacion");
-
-return (
-<div>
-
-<nav style={{display:"flex", gap:"10px"}}>
-<button onClick={()=>setPantalla("evaluacion")}>Evaluar</button>
-<button onClick={()=>setPantalla("historial")}>Historial</button>
-</nav>
-
-{pantalla==="evaluacion" && <Evaluacion/>}
-{pantalla==="historial" && <Historial/>}
-
-</div>
-);
+  return <Evaluacion />;
 }
